a5a9f343ea2012874fa9a543badd7d28f9d5213b
Miroshnikov <MiroshnikovLI@yandex.ru>
2025-08-23 11:58:54 +0300

    добавлен jsDocs

--- src/components/ui/dynamic-grid/dynamic-grid.tsx
+++ src/components/ui/dynamic-grid/dynamic-grid.tsx
@@ -2,6 +2,17 @@ import { FC } from "react";
 import styles from './dynamic-grid.module.scss'
 import IDynamicGridUI from "./dynamic-grid.interface";
 
+/**
+ * @component Компонент динамической сетки UI.
+ * @param {object} props - Свойства компонента.
+ * @param {React.CSSProperties} [props.style] - Стиль для контейнера.
+ * @param {function} [props.onCellClick] - Обработчик клика по ячейке.
+ * @param {function} [props.onContextMenu] - Обработчик контекстного меню.
+ * @param {React.ReactNode} [props.children] - Дочерние элементы внутри компонента.
+ * @param {string} [props.className] - Дополнительные классы для контейнера.
+ * 
+ * @returns {JSX.Element} - Элемент JSX компонента.
+ */
 const DynamicGridUI: FC<IDynamicGridUI> = ({ style, onCellClick, onContextMenu, children, className }) => {
     return (
         <div
